35eb82016b1228b0b215d2ae45a1c258e93c10bb
Ammon Werner <ammon@altafin.co>
2022-05-29 22:04:50 -0600

    update package.json

--- src/App.js
+++ src/App.js
@@ -28,6 +28,21 @@ function App() {
     return () => clearTimeout(timer);
   }, []);
 
+  const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
   return (
     <Router>
       <Preloader load={load} />
